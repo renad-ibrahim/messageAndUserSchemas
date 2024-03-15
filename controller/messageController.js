3777db98580cfc194bafcf20978fc67ca365a57c
Renad Ibrahim <78569148+renad-ibrahim@users.noreply.github.com>
2024-03-15 21:41:18 +0200

    Update messageController.js

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,14 +1,13 @@
-const messageModel = require("../model/messageModel");
-
+const Message = require("../models/messageModel");
 //createMessage
-const createMessage = async(req,res)=>{
-    const {chatId , senderId ,receiverId,createdAt, content} = req.body
+const createMessage = async (req, res) => {
+    const { content } = req.body
 
-    const message = new messageModel({chatId , senderId ,receiverId,createdAt, content})
-    // if(!content || !senderId || !receiverId ||chatId){
-    //     console.log("Invalid content");
-    //     return res.sendStatus(400)
-    // }
+    const message = new Message({
+        sender: req.user._id,
+        content: content,
+
+    })
     try {
         const response = await message.save()
         res.status(200).json(response)
@@ -18,25 +17,26 @@ const createMessage = async(req,res)=>{
             status: "error",
             message: "Failed to create message",
             error: error.message
-    })
-}
+        })
+    }
 }
 //get message
-const getMessage = async (req,res)=>{
-    const {chatId} = req.params;
+const getMessage = async (req, res) => {
+    const { chatId } = req.params;
 
     try {
-        const message = await messageModel.find({chatId})
-
+        const message = await messageModel.find({ chatId })
         res.status(200).json(message)
     } catch (error) {
         console.error("Failed to get messages:", error);
-        res.status(400).json({
-            status: "error",
-            message: "Failed to get messages",
-            error: error.message 
-        });
+        res.status(400).json
+            (
+                {
+                    status: "error",
+                    message: "Failed to get messages",
+                    error: error.message
+                });
     }
 }
 
-module.exports = {createMessage , getMessage}
+module.exports = { createMessage, getMessage }
